9ef5c5c4626b49fe9207ce8d4bc6e8e99c980b40
blue <blue@lcm.mi.infn.it>
2015-07-10 15:08:39 +0200

    preparing module for insertion into ROOT
    - moved .cxx and .h files in src and inc directories
    - created first version of CMakeLists.txt and Module.mk
    - removed setup.cxx and makefile
    - moved test directory out of the folder

--- setup.cxx
+++
@@ -1,8 +0,0 @@
-void setup()
-{
-   gSystem->CompileMacro("TJob.cxx");
-   gSystem->CompileMacro("TNote.cxx");
-   gSystem->CompileMacro("TServer.cxx");
-   gSystem->CompileMacro("TMultiProcess.cxx");
-   gSystem->CompileMacro("TPool.cxx");
-}
--- test/myJob.cxx
+++
@@ -1,9 +0,0 @@
-#include "myJob.h"
-#include "TH1F.h"
-
-TObject *myJob::Process()
-{
-   TH1F *h = new TH1F("h1", "h", 100, -3, 3);
-   h->FillRandom("gaus", 10000);
-   return h;
-}
